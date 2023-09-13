82dd3b9efd070c11bf20049d542eb04d64161830
Luiz Gabriel Pessanha <luizpessanhaofficial@gmail.com>
2023-09-13 18:47:49 -0300

    var hora real

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 12
+    var hora = data.getHours()
     var min = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas.`
 

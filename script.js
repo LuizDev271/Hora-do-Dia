40c03b0d560edcae433d83300d796c89e9645af1
Luiz Gabriel Pessanha <luizpessanhaofficial@gmail.com>
2023-09-13 18:33:57 -0300

    add madruga

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 
     var min = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas.`
 
         if (min < 10){
             min = '0' + min
         }
+        
+        if (hora < 10){
+            hora = '0' + hora
+        }
     
         if (hora == 1){
-            msg.innerHTML = `Agora são ${hora}:${min} Hora da manhã.`
+            msg.innerHTML = `Agora são ${hora}:${min} Hora da madrugada.`
             document.body.style.backgroundColor = '#ad8978'
-        } else if (hora >= 0 && hora < 12){
+        } else if (hora >= 0 && hora < 6){
+            //boa madrugada
+            msg.innerHTML = `Agora são ${hora}:${min} Horas da madrugada.`
+            img.src = 'imagens/madrugada.png'
+            document.body.style.backgroundColor = '#000128'
+        } else if (hora >= 6 && hora < 12){
             //bom dia
             msg.innerHTML = `Agora são ${hora}:${min} Horas da manhã.`
             img.src = 'imagens/manha.png'

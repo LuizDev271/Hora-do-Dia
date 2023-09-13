da46cccc60389a340e157e9a74ea1185b4628454
Luiz Gabriel Pessanha <luizpessanhaofficial@gmail.com>
2023-09-13 18:46:40 -0300

    correção h1

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 12
     var min = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas.`
 
@@ -15,27 +16,31 @@ function carregar(){
         }
     
         if (hora == 1){
-            document.getElementsByTagName('h1')[0].innerText = 'Hora da Noite'
+            document.getElementsByTagName('h1')[0].innerText = 'Hora da Madrugada'
             msg.innerHTML = `Agora são ${hora}:${min} Hora da madrugada.`
             img.src = 'imagens/madrugada.png'
             document.body.style.backgroundColor = '#000128'
         } else if (hora >= 0 && hora < 6){
             //boa madrugada
-            msg.innerHTML = `Agora são ${hora}:${min} Horas da madrugada.`
+            document.getElementsByTagName('h1')[0].innerText = 'Hora da Madrugada'
+            msg.innerHTML = `Agora são ${hora}:${min} Horas da madrugadas.`
             img.src = 'imagens/madrugada.png'
             document.body.style.backgroundColor = '#000128'
         } else if (hora >= 6 && hora < 12){
             //bom dia
+            document.getElementsByTagName('h1')[0].innerText = 'Hora da Manhã'
             msg.innerHTML = `Agora são ${hora}:${min} Horas da manhã.`
             img.src = 'imagens/manha.png'
             document.body.style.backgroundColor = '#ad8978'
         } else if (hora >= 12 && hora < 18){
             //boa tarde
+            document.getElementsByTagName('h1')[0].innerText = 'Hora da Tarde'
             msg.innerHTML = `Agora são ${hora}:${min} Horas da tarde.`
             img.src = 'imagens/tarde.png'
             document.body.style.backgroundColor = '#ed9748'
         }  else if (hora >= 18){
             //boa noite
+            document.getElementsByTagName('h1')[0].innerText = 'Hora da Noite'
             msg.innerHTML = `Agora são ${hora}:${min} Horas da noite.`
             img.src = 'imagens/noite.png'
             document.body.style.backgroundColor = '#4d3553'
